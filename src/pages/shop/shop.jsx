281c2e8c77726b7f8515d67e2cb599797186d60f
chris <turabayochryso98@gmail.com>
2023-10-04 19:17:26 +0900

    first commit

---
+++ src/pages/shop/shop.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { PRODUCTS } from "../../products";
+import { Product } from "./product";
+import "./shop.css"
+
+export const Shop = () => {
+    return (
+      <div className="shop">
+        <div className="shopTitle">
+          <h1>Chris E-shop</h1>
+        </div>
+  
+        <div className="products">
+          {PRODUCTS.map((product) => (
+            // create a prop
+            <Product data={product} />
+          ))}
+        </div>
+        
+      </div>
+    );
+  };
